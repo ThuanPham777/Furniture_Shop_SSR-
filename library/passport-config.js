d11d39c3f90628285222731783d7ab5ee8deebca
ThuanPham777 <phamchithuan777@gmail.com>
2024-11-24 23:12:17 +0700

    change callbackUrl google

--- library/passport-config.js
+++ library/passport-config.js
@@ -48,7 +48,8 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL:
+          'https://furniture-shop-raqg.onrender.com/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
